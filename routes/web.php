d70fdc680702d020894413ba6abc2e55613628b7
Chyrakn <needlle@mail.ru>
2019-12-15 16:34:48 +0300

    create users table

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function(){
     return view('welcome');
 });
+
+Route::get('/endru', function(){
+    return view('welcome');
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
